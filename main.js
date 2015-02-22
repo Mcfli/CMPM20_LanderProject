df03045e8150a0b0f849f838690327b02ff61149
Enthrayu <atu4@ucsc.edu>
2015-02-21 16:10:27 -0800

    Potential Fonts to use

--- main.js
+++ main.js
@@ -1,185 +1,42 @@
-// Phaser Lander Test
-// main.js
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'lander', {preload:preload, create:create, update:update, render:render});
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, update: update });
+var text;
+var titletext;
 
-function preload(){
-	game.load.image('ship', 'assets/ship_2.png');
-	game.load.image('platform', 'assets/platform.png');
-	game.load.image('planet', 'assets/circle.png');
-	game.load.image('square', 'assets/square.png');
-	game.load.audio('jupiter', 'assets/jupiter.ogg');
+function create() {
+	game.stage.setBackgroundColor(255);
+    text = game.add.txext(game.world.centerX, 150, 'Touchdown Adventures \n             of the Philae Lander');
+    text.anchor.set(0.5);
+    text.align = 'center';
+    text.font = 'Arial';
+    text.fontSize = 40;
+    text.fontWeight = 'bold';
+    text.fill = 'gold';
+    text.setShadow(0, 0, 'rgba(0, 0, 0, 0.4)', 0);	
+
+	titletext = game.add.text(game.world.centerX, 500, 'New Game \n Load Game \n Options');
+	titletext.anchor.set(0.5);
+	titletext.align = 'center';
+	titletext.font = 'Arial Black';
+	titletext.fontSize = 20;
+	titletext.fill = 'white';
 }
 
-var lander;
-var planet;
-var platform;
-var gravPoint;
-var movingBox;
-var tiltBox;
-var cursors;
 
-function create(){
-	music = game.add.audio('jupiter');
-    music.play();
-    
-    //setting world size (larger than canvas)
-    game.world.setBounds(0, 0, 2000, 2000);    
-    
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.physics.startSystem(Phaser.Physics.P2JS);
-	
-	game.physics.p2.setImpactEvents(true);
-	game.physics.p2.restitution = 0.8;
-	
-	game.stage.backgroundColor = '#2d2d2d';
-	
-	planet = game.add.sprite(1000,1000, 'planet');
-	planet.scale.setTo(2);
-	planet.anchor.set(0.5);
-	var planetW = planet.width;
-	var planetH = planet.height;
-	game.physics.p2.enable(planet, true);
-	planet.body.static = true;
-	planet.body.setCircle(160);
-	planet.enableBody = true;
-	
-	// point where gravity moves toward (center of planet)
-	gravPoint = new Phaser.Point(planet.x, planet.y);
-	
-	platform = game.add.sprite(planet.x,planet.y - 160, 'platform');
-	platform.anchor.set(0.5);
-	platform.scale.setTo(0.5);
-	game.physics.p2.enable(platform, true);
-	platform.body.static = true;
-	platform.enableBody = true;
-	
-	lander = game.add.sprite(100,50,'ship');
-	lander.scale.setTo(0.5,0.5);
-	lander.anchor.set(0.5);
-	game.physics.p2.enable(lander, true);
-	
-	// Obstacle wall using array
-	var obstArray = new Array();
-	for(var i = 0; i < 8; i++){
-		if(i == 0) obstArray.push(game.add.sprite(800, 700, 'square'));
-		else obstArray.push(game.add.sprite(obstArray[i - 1].x + 50, obstArray[0].y, 'square'));
-		obstArray[i].anchor.set(0.5);
-		obstArray[i].scale.setTo(0.75,0.75);
-		game.physics.p2.enable(obstArray[i], true);
-		obstArray[i].body.static = true;
-		obstArray[i].enableBody = true;
-	}
-	
-	// Moving box
-	movingBox = game.add.sprite(800, 780, 'square');
-	movingBox.anchor.set(0.5);
-	movingBox.scale.setTo(0.75,0.75);
-	game.physics.p2.enable(movingBox, true);
-	movingBox.enableBody = true;
-	velocityTowards(movingBox,gravPoint,80);
-	movingBox.body.kinematic = true;
-	
-	tiltBox = game.add.sprite(1300, 750, 'square');
-	tiltBox.anchor.set(0.5);
-	tiltBox.scale.setTo(0.75,0.75);
-	game.physics.p2.enable(tiltBox, true);
-	tiltBox.enableBody = true;
-	velocityTowards(tiltBox,gravPoint,80);
-	tiltBox.body.kinematic = true;
-	
-	// Calls for obstacles moving at the same speed, only need 1 call
-	game.time.events.loop(Phaser.Timer.SECOND * 2.25, flipVel);
-
-	// Create collision groups
-	var landerCollisionGroup = game.physics.p2.createCollisionGroup();
-	var planetCollisionGroup = game.physics.p2.createCollisionGroup();
-	var platformCollisionGroup = game.physics.p2.createCollisionGroup();
-	var obstacleCollisionGroup = game.physics.p2.createCollisionGroup();
-	
-	// Collidie with world bounds
-	game.physics.p2.updateBoundsCollisionGroup();
-	
-	// Assign collision groups
-	planet.body.setCollisionGroup(planetCollisionGroup);
-	platform.body.setCollisionGroup(platformCollisionGroup);
-	lander.body.setCollisionGroup(landerCollisionGroup);
-	for(var i = 0; i < obstArray.length; i++){
-		obstArray[i].body.setCollisionGroup(obstacleCollisionGroup);
-		obstArray[i].body.collides([obstacleCollisionGroup, landerCollisionGroup]);
-	}
-	movingBox.body.setCollisionGroup(obstacleCollisionGroup);
-	tiltBox.body.setCollisionGroup(obstacleCollisionGroup);
-	
-	// Object collisions
-	planet.body.collides([planetCollisionGroup,landerCollisionGroup]);
-	platform.body.collides([platformCollisionGroup,landerCollisionGroup]);
-	movingBox.body.collides([obstacleCollisionGroup, landerCollisionGroup]);
-	tiltBox.body.collides([obstacleCollisionGroup, landerCollisionGroup]);
-	
-	lander.body.collides(platformCollisionGroup,landerHit,this);
-	lander.body.collides(obstacleCollisionGroup);
-	lander.body.collides(planetCollisionGroup);
-	
-	//sets camera to follow lander sprite
-	game.camera.follow(lander);
-	
-	// Arrow keys
-	cursors = game.input.keyboard.createCursorKeys();
-	
-	// specifies function to call when collision occurs
-	lander.onBeginContact.add(landerHit,this);
-	
+function update() {
+	var offset = moveToXY(game.input.activePointer, text.x, text.y, 8);
+    text.setShadow(offset.x, offset.y, 'rgba(0, 0, 0, 0.5)', distanceToPointer(text, game.input.activePointer) / 30);
 }
 
-function update(){
-	if(cursors.up.isDown){
-		lander.body.thrust(100);
-	}
-	if(cursors.left.isDown){
-		lander.body.rotateLeft(100);
-	}
-	else if(cursors.right.isDown){
-		lander.body.rotateRight(100);
-	}
-	else{
-		lander.body.setZeroRotation();
-	}
-	
-	accelerateToObject(lander,gravPoint,50);
+
+function distanceToPointer(displayObject, pointer) {
+    this._dx = displayObject.x - pointer.x;
+    this._dy = displayObject.y - pointer.y;
+    return Math.sqrt(this._dx * this._dx + this._dy * this._dy);
 }
 
-function accelerateToObject(obj1, obj2, speed){
-	if(typeof speed === 'undefined'){speed = 50;}
-	var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-    obj1.body.force.x += Math.cos(angle) * speed;
-    obj1.body.force.y += Math.sin(angle) * speed;
-}
-
-//function called by collision
-function landerHit(body, shapeA, shapeB, equation){
-		lander.body.static = true;
-}
-
-// Add every obstacle that needs to move at the same time
-function flipVel(){
-	reverseVel(movingBox);
-	reverseVel(tiltBox);
-}
-
-// Called by timer function to reverse object's velocity
-function reverseVel(obj1){
-	obj1.body.velocity.y = -obj1.body.velocity.y;
-	obj1.body.velocity.x = -obj1.body.velocity.x;
-}
-
-function velocityTowards(obj1, obj2, speed){
-	if(typeof speed === 'undefined'){speed = 80;}
-	var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-	obj1.body.rotation = angle;
-    obj1.body.velocity.x = Math.cos(angle) * speed;
-    obj1.body.velocity.y = Math.sin(angle) * speed;
-}
-
-function render(){
-	game.debug.bodyInfo(lander,32,32);
+function moveToXY(displayObject, x, y, speed) {
+    var _angle = Math.atan2(y - displayObject.y, x - displayObject.x);    
+    var x = Math.cos(_angle) * speed;
+    var y = Math.sin(_angle) * speed;
+    return { x: x, y: y };
 }
