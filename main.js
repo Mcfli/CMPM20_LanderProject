ebf8ca71b78c7f5cd3fa329f1c80988329b771e2
Enthrayu <atu4@ucsc.edu>
2015-02-24 16:20:57 -0800

    Menu
    Working Fonts!

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, update: update });
-var text;
-var titletext;
+var backgroundTest;
+
+//  The Google WebFont Loader will look for this object, so create it before loading the script.
+WebFontConfig = {
+
+    //  'active' means all requested fonts have finished loading
+    //  We set a 1 second delay before calling 'createText'.
+    //  For some reason if we don't the browser cannot render the text the first time it's created.
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+    //  The Google Fonts we want to load (specify as many as you like in the array)
+    google: {
+      families: ['BANGERS','Orbitron']
+    }
+};
+function preload(){
+	//  Load the Google WebFont Loader script
+    game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+
+}
+
+var text = null;
+var titletext = null;
+var grd;
 
 function create() {
-	game.stage.setBackgroundColor(255);
-    text = game.add.txext(game.world.centerX, 150, 'Touchdown Adventures \n             of the Philae Lander');
+//Centers the canvas	
+	this.game.scale.pageAlignHorizontally = true;
+	this.game.scale.pageAlignVertically = true;
+	this.game.scale.refresh();
+
+	game.stage.setBackgroundColor('0x2d2d2d');
+	text = game.add.text(game.world.centerX, 100, "Touchdown Adventures of \n          the Philae Lander");
     text.anchor.set(0.5);
-    text.align = 'center';
-    text.font = 'Arial';
+    
+    text.font = 'BANGERS';
     text.fontSize = 40;
     text.fontWeight = 'bold';
     text.fill = 'gold';
     text.setShadow(0, 0, 'rgba(0, 0, 0, 0.4)', 0);	
 
+	titletext = 'Orbitron';
 	titletext = game.add.text(game.world.centerX, 500, 'New Game \n Load Game \n Options');
 	titletext.anchor.set(0.5);
 	titletext.align = 'center';
-	titletext.font = 'Arial Black';
 	titletext.fontSize = 20;
 	titletext.fill = 'white';
 }
